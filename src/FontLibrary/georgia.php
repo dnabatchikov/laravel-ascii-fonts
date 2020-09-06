8bb644944839ab855b103e0cddd6813e86d7c372
Denis Nabatchikov <denis.nabatchikov@skillbox.ru>
2020-09-06 21:21:05 +0300

    some font

---
+++ src/FontLibrary/georgia.php
@@ -0,0 +1,33 @@
+<?php
+$rep = str_repeat(' ', 14);
+
+return [
+    'kegle' => 9,
+    'A'     => ['      db      ', '     ;MM:     ', '    ,V^MM.    ', '   ,M  `MM    ', '   AbmmmqMA   ', "  A'     VM   ", '.AMA.   .AMMA.', $rep, $rep, $rep],
+    'B'     => ['`7MM"""Yp, ', '  MM    Yb ', '  MM    dP ', '  MM"""bg. ', '  MM    `Y ', '  MM    ,9 ', '.JMMmmmd9  ', $rep, $rep, $rep],
+    'C'     => ['  .g8"""bgd ', ".dP'     `M ", "dM'       ` ", 'MM          ', 'MM.         ', "`Mb.     ,' ", "  `\"bmmmd'  ", $rep, $rep, $rep],
+    'D'     => ['`7MM"""Yb.   ', '  MM    `Yb. ', '  MM     `Mb ', '  MM      MM ', '  MM     ,MP ', "  MM    ,dP' ", ".JMMmmmdP'   ", $rep, $rep, $rep],
+    'E'     => ['`7MM"""YMM  ', '  MM    `7  ', '  MM   d    ', '  MMmmMM    ', '  MM   Y  , ', '  MM     ,M ', '.JMMmmmmMM ', $rep, $rep, $rep],
+    'F'     => ['`7MM"""YMM  ', '  MM    `7  ', '  MM   d    ', '  MMmmMM    ', '  MM   Y    ', '  MM        ', '.JMML.      ', $rep, $rep, $rep],
+    'G'     => ['  .g8"""bgd  ', ".dP'     `M  ", "dM'       `  ", 'MM           ', "MM.    `7MMF'", '`Mb.     MM  ', '  `"bmmmdPY  ', $rep, $rep, $rep],
+    'H'     => ["`7MMF'  `7MMF'", '  MM      MM  ', '  MM      MM  ', '  MMmmmmmmMM  ', '  MM      MM  ', '  MM      MM  ', ' .JMML.  .JMML.', $rep, $rep, $rep],
+    'I'     => [" `7MMF' ", '   MM   ', '   MM   ', '   MM   ', '   MM   ', '   MM   ', ' .JMML. ', $rep, $rep, $rep],
+    'J'     => ["   `7MMF'", '     MM  ', '     MM  ', '     MM  ', '     MM  ', '(O)  MM  ', ' Ymmm9   ', $rep, $rep, $rep],
+    'K'     => ["`7MMF' `YMM' ", "  MM   .M'   ", '  MM .d"     ', '  MMMMM.     ', '  MM  VMA    ', '  MM   `MM.  ', '.JMML.   MMb.', $rep, $rep, $rep],
+    'L'     => ["`7MMF'      ", '  MM        ', '  MM        ', '  MM        ', '  MM      , ', '  MM     ,M ', '.JMMmmmmMMM ', $rep, $rep, $rep],
+    'M'     => ["`7MMM.     ,MMF'", '  MMMb    dPMM  ', '  M YM   ,M MM  ', "  M  Mb  M' MM  ", "  M  YM.P'  MM  ", '  M  `YM\'   MM  ', ".JML. `'  .JMML.", $rep, $rep, $rep],
+    'N'     => ["`7MN.   `7MF'", '  MMN.    M  ', '  M YMb   M  ', '  M  `MN. M  ', '  M   `MM.M  ', '  M     YMM  ', '.JML.    YM  ', $rep, $rep, $rep],
+    'O'     => ['  .g8""8q.   ', ".dP'    `YM. ", "dM'      `MM ", 'MM        MM ', 'MM.      ,MP ', "`Mb.    ,dP' ", "  `\"bmmd\"'   ", $rep, $rep, $rep],
+    'P'     => ['`7MM"""Mq. ', '  MM   `MM.', '  MM   ,M9 ', '  MMmmdM9  ', '  MM       ', '  MM       ', '.JMML.     ', $rep, $rep, $rep],
+    'Q'     => ['  .g8""8q.   ', ".dP'    `YM. ", "dM'      `MM ", 'MM        MM ', 'MM.      ,MP ', "`Mb.    ,dP' ", "  `\"bmmd\"'   ", '      MMb   ', "       `bood'"],
+    'R'     => ['`7MM"""Mq.  ', '  MM   `MM. ', '  MM   ,M9  ', '  MMmmdM9   ', '  MM  YM.   ', '  MM   `Mb. ', '.JMML. .JMM.', $rep, $rep, $rep],
+    'S'     => [' .M"""bgd ', ',MI    "Y ', '`MMb.     ', '  `YMMNq. ', '.     `MM ', 'Mb     dM ', 'P"Ybmmd"  ', $rep, $rep, $rep],
+    'T'     => ['MMP""MM""YMM ', "P'   MM   `7 ", '     MM      ', '     MM      ', '     MM      ', '     MM      ', '   .JMML.    ', $rep, $rep, $rep],
+    'U'     => ["`7MMF'   `7MF'", '  MM       M  ', '  MM       M  ', '  MM       M  ', '  MM       M  ', '  YM.     ,M  ', "   `bmmmmd\"'  ", $rep, $rep, $rep],
+    'V'     => ["`7MMF'   `7MF'", '  `MA     ,V  ', '   VM:   ,V   ', "    MM.  M'   ", "    `MM A'    ", '     :MM;     ', '      VF      ', $rep, $rep, $rep],
+    'W'     => ["`7MMF'     A     `7MF'", '  `MA     ,MA     ,V  ', '   VM:   ,VVM:   ,V   ', "    MM.  M' MM.  M'   ", "    `MM A'  `MM A'    ", '     :MM;    :MM;     ', '      VF      VF      ', $rep, $rep, $rep],
+    'X'     => ["`YMM'   `MP' ", '  VMb.  ,P   ', "   `MM.M'    ", '     MMb     ', "   ,M'`Mb.   ", '  ,P   `MM.  ', '.MM:.  .:MMa.', $rep, $rep, $rep],
+    'Y'     => ["`YMM'   `MM'", '  VMA   ,V  ', '   VMA ,V   ', '    VMMP    ', '     MM     ', '     MM     ', '   .JMML.   ', $rep, $rep, $rep],
+    'Z'     => ['MMM"""AMV ', "M'   AMV  ", "'   AMV   ", '   AMV    ', '  AMV   , ', ' AMV   ,M ', 'AMVmmmmMM ', $rep, $rep, $rep],
+
+];
