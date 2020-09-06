4429c1366d361d32a310f4d549292f9d6897fa67
Denis Nabatchikov <denis.nabatchikov@skillbox.ru>
2020-09-06 21:21:39 +0300

    MainClassTests

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,6 +2,7 @@
 
 namespace Dnabatchikov\LaravelAsciiFonts\Tests;
 
+use Dnabatchikov\LaravelAsciiFonts\LaravelAsciiFonts;
 use Orchestra\Testbench\TestCase;
 use Dnabatchikov\LaravelAsciiFonts\LaravelAsciiFontsServiceProvider;
 
@@ -12,10 +13,40 @@ class ExampleTest extends TestCase
     {
         return [LaravelAsciiFontsServiceProvider::class];
     }
-    
-    /** @test */
-    public function true_is_true()
+
+    /**
+     * @dataProvider dP
+     * @test
+     *
+     * @param $actual
+     * @param $expected
+     */
+    public function validValueTest($actual, $expected): void
     {
-        $this->assertTrue(true);
+        self::assertSame(md5($this->r($expected)), md5($this->r($actual)));
+    }
+
+    private function r(string $text)
+    {
+        return str_replace(' ', '', $text);
+    }
+
+    public function dP(): array
+    {
+        return [
+            [
+                'expected' => md5($this->r("      db      `7MMM.     ,MMF'  .g8\"\"8q.   `7MM\"\"\"Mq.
+     ;MM:       MMMb    dPMM  .dP'    `YM.   MM   `MM.
+    ,V^MM.      M YM   ,M MM  dM'      `MM   MM   ,M9
+   ,M  `MM      M  Mb  M' MM  MM        MM   MMmmdM9
+   AbmmmqMA     M  YM.P'  MM  MM.      ,MP   MM
+  A'     VM     M  `YM'   MM  `Mb.    ,dP'   MM
+.AMA.   .AMMA..JML. `'  .JMML.  `\"bmmd\"'   .JMML.
+                                  MMb
+                                   `bood'
+")),
+                'actual'   => md5($this->r((new LaravelAsciiFonts('georgia', 'AMQP'))->display())),
+            ],
+        ];
     }
 }
